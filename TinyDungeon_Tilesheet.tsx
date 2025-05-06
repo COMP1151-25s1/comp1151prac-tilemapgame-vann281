38f8ab5ad2598d9b62ae5f4bdc43ff07fcf8a896
VannRobens <vann.robens@students.mq.edu.au>
2025-05-07 09:33:58 +1000

    tilemap setup

---
+++ TinyDungeon_Tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TinyDungeon_Tilesheet" tilewidth="16" tileheight="16" tilecount="132" columns="12">
+ <image source="//claudius.science.mq.edu.au/49148923/.Desktop/Tilemap/tilemap.png" width="203" height="186"/>
+</tileset>
