2a8671abe0b5dc2becc12a2121fbb74572301c52
VannRobens <vann.robens@students.mq.edu.au>
2025-05-07 10:49:24 +1000

    ripping in

---
+++ Assets/Tiled/tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tilemap" tilewidth="16" tileheight="16" tilecount="132" columns="12">
+ <image source="//claudius.science.mq.edu.au/49148923/.Desktop/tilemap_packed.png" width="192" height="176"/>
+</tileset>
